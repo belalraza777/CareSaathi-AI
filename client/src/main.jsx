b2ff3ac6f6c3a886c4fe6873ae1ea796f58f7358
belal raza <belalraza707@gmail.com>
2026-07-16 11:23:31 +0530

    check for prod in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from './context/authContext'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import { AuthProvider } from './context/authContext';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = (
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* Provides SPA routing context across all app screens. */}
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>,
-)
+  isProduction ? app : <StrictMode>{app}</StrictMode>
+);
